1d409c9035695d4773e9eaff6f99f81fcb56c452
Baltha <adrien.bellonserre@hotmail.fr>
2014-02-13 11:04:50 +0100

    Affichage détail produit Amazon/RdC
    amélioration regex

--- app/Models/App_model.php
+++ app/Models/App_model.php
@@ -44,60 +44,33 @@ class App_model extends Model{
       preg_match('/id="actualPriceValue"\>\s*\<b[^>]*\>\s*([^<]*)\s*/is', $homepage, $matchesPrice);
       preg_match('/class="productDescriptionWrapper"\>\s*(.*?)\s*\<div class="emptyClear"/is', $homepage, $matchesDescribe);
       preg_match('/<img id="main-image-nonjs" src="(.*?)"/is', $homepage, $matchesPicture);
+      $nom=$matchesName[1];
+      $price=$matchesPrice[1];
+      $describe=$matchesDescribe[1];
+      $picture=$matchesPicture[1];
+      $tabResult=array('nom'=>$nom,'price'=>$price,'describe'=>$describe,'picture'=>$picture);
       //echo "<img src=".$matchesPicture[0]."><br />";
     }
     //RueDuCommerce
     else if(preg_match('#rueducommerce#', $params['product']))
     {
       echo 'RUE DU COMMERCE'.'<br />';
-      preg_match('/class="brandNameSpan"\>\s*(.*?)\<\/div\>/is', $homepage, $matchesName);
-      preg_match('/class="newPrice"\>\s*(.*?)\<\/span\>/is', $homepage, $matchesPrice);
-      preg_match('/class="ficheProduit_descriptionCourte"\>\s*(.*?)\<\/div\>/is', $homepage, $matchesDescribe);
-      preg_match('/class="photo"\>\s*(.*?)\<\/div\>/is', $homepage, $matchesPicture);
+      preg_match('/class="brandNameSpan"\>\s*\<a href=".*?"\>(.*?)\<\/a\>\s*<\/span\>\s*(.*?)\<\/h1\>/is', $homepage, $matchesName);
+      preg_match('/class="newPrice"\>\s*(.*?)\<sup\>\s*(.*?)\<\/sup\>/is', $homepage, $matchesPrice);
+      preg_match('/class="ficheProduit_descriptionCourte"\>\s*(.*?)([^<br>]\s*[^<br \/>]*)\<\/div\>/is', $homepage, $matchesDescribe);
+      preg_match('/class="photo"\>\s*\<a id="linkPhoto" target="_blank" href="(.*?)"\>/is', $homepage, $matchesPicture);
       $like =file_get_contents("http://graph.facebook.com/?id=".$params['product']);
       $like=json_decode($like);
+      $nom =$matchesName[1].$matchesName[2];
+      $price=$matchesPrice[1].$matchesPrice[2];
+      $describe=$matchesDescribe[1];
+      $picture=$matchesPicture[1];
+
+      $tabResult=array('like'=>$like->{'shares'},'nom'=>$nom,'price'=>$price,'describe'=>$describe,'picture'=>$picture);
      // echo "Nb Like Facebook : ".$like->{'shares'}."<br />";
      // echo "<div style=width:500px;height:500px>".$matchesPicture[1] ."</div><br />";
     }
-    
-      //Affichage de l'ensemble des résultats (excepte Les likes/et les photos)
-      $nom=$matchesName[1];
-      $price=$matchesPrice[1];
-      $describe=$matchesDescribe[1];
-          
-      //echo $nom ."<br />";
-      //echo $price ."<br />";
-      //echo $describe ."<br />";
-      $tabResult=array('nom'=>$nom);
       return $tabResult;
   }
-  // function getUsers($params){
-  //   return $this->getMapper('wifiloc')->find(array('promo=?',$params['promo']),array('order'=>'lastname'));
-  // }
-  
-  // function getUser($params){
-  //   return $this->getMapper('wifiloc')->load(array('userId=?',$params['name']));
-  // }
-  
-  // function searchUsers($params){
-  //   $query='(firstname like "%'.$params['keywords'].'%" or lastname like "%'.$params['keywords'].'%")';
-  //   $query.=$params['filter']?' and promo="'.$params['filter'].'"':'';
-  //   return $this->getMapper('wifiloc')->find($query);
-  // }
-  // public function favorite($params){
-  //   $map=$this->getMapper('wififav');
-  //   $favorite=$map->load(array('favId=? and logId=?',$params['favId'],$params['logId']));
-  //   if(!$favorite){
-  //     $map->favId=$params['favId'];
-  //     $map->logId=$params['logId'];
-  //     $map->save();
-  //     return true;
-  //   }else{
-  //     $favorite->erase();
-  //     return false;
-  //   }
-
-  //  }
-  
 }
 ?>
